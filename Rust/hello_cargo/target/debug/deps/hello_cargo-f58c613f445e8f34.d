f83f4a1aa4d3fcdc129ebb59f3fdfa796466d0c5
Nuggyyy <121103879+Nuggyyy@users.noreply.github.com>
2026-03-10 18:02:05 +0100

    added rust stuff from macbook

---
+++ Rust/hello_cargo/target/debug/deps/hello_cargo-f58c613f445e8f34.d
@@ -0,0 +1,5 @@
+/Users/seraf/Documents/Hobby/temp/hello_cargo/target/debug/deps/hello_cargo-f58c613f445e8f34.d: src/main.rs
+
+/Users/seraf/Documents/Hobby/temp/hello_cargo/target/debug/deps/hello_cargo-f58c613f445e8f34: src/main.rs
+
+src/main.rs:
---
+++ Rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/seraf/Documents/Hobby/temp/hello_cargo/target/debug/hello_cargo: /Users/seraf/Documents/Hobby/temp/hello_cargo/src/main.rs
